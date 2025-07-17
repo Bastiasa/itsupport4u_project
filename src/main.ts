01056a4f94f66ed5e8c6c7e26385719d5b8afa2d
Bastiasa <leysantqm@gmail.com>
2025-07-17 17:02:32 -0500

    Refactor scrapping functions to streamline badge extraction from Credly and Accredible

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,28 @@ import puppeteer, { Browser } from "puppeteer";
 import fs from 'fs/promises';
 import { resolve } from "path";
 
+// Target websites
 const FARAZ_AHMAD_CREDLY = "https://www.credly.com/users/faraz-ahmad.a5935bd3/badges#credly";
 const FARAZ_AHMAD_WALLET = "https://www.credential.net/profile/farazahmad283861/wallet";
 
+// Classes of credly
 const CREDLY_CERTIFICATION_CONTAINER_SELECTOR = ".Cardstyles__StyledContainer-fredly__sc-1yaakoz-0.fRJHRP.EarnedBadgeCardstyles__StyledCard-fredly__sc-gsqjwh-1.jwtiVz";
 const CREDLY_CERTIFICATION_IMAGE_SELECTOR = ".EarnedBadgeCardstyles__ImageContainer-fredly__sc-gsqjwh-0.dDMlBy"
 const CREDLY_CERTIFICATION_NAME_SELECTOR = ".Typographystyles__Container-fredly__sc-1jldzrm-0.enJnLg.EarnedBadgeCardstyles__BadgeNameText-fredly__sc-gsqjwh-7.hqrelZ"
 
-const WALLET_CERTIFICATION_CONTAINER_SELECTOR = ".wallet-container .tile-gallery .cdk-drop-list.tile.cdk-drop-list-disabled.ng-star-inserted"
-const WALLET_CERTIFICATION_CUSTOM_CREDENTIAL_COVER_ELEMENT = ".custom-credential.ng-star-inserted.ng-lazyloaded"
-const WALLET_CERTIFICATION_SCREENSHOT_COVER_SELECTOR = `${WALLET_CERTIFICATION_CONTAINER_SELECTOR} .certificate.us-letter.default.landscape`;
-const WALLET_CERTIFICATION_IMAGE_SELECTOR = `svg image, ${WALLET_CERTIFICATION_CONTAINER_SELECTOR} img.ng-lazyloaded`;
-const WALLET_CERTIFICATION_NAME_SELECTOR = ".details .mat-h3";
-const WALLET_CERTIFICATION_LINK_SELECTOR = `${WALLET_CERTIFICATION_CONTAINER_SELECTOR} a.ng-star-inserted`
+// Classes of Accredible
+const ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR = ".wallet-container .tile-gallery .cdk-drop-list.tile.cdk-drop-list-disabled.ng-star-inserted"
+const ACCREDIBLE_CERTIFICATION_CUSTOM_CREDENTIAL_COVER_ELEMENT = ".custom-credential.ng-star-inserted.ng-lazyloaded"
+const ACCREDIBLE_CERTIFICATION_SCREENSHOT_COVER_SELECTOR = `${ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR} .certificate.us-letter.default.landscape`;
+const ACCREDIBLE_CERTIFICATION_IMAGE_SELECTOR = `svg image, ${ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR} img.ng-lazyloaded`;
+const ACCREDIBLE_CERTIFICATION_NAME_SELECTOR = ".details .mat-h3";
+const ACCREDIBLE_CERTIFICATION_LINK_SELECTOR = `${ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR} a.ng-star-inserted`
 
-
+// Arguments from the console
 const execArgv = process.argv.slice(2);
 
 
+// Scrapping Credly badges 
 async function scrapCredly(browser: Browser) {
 
     console.log("Starting credly scrap...");
@@ -28,14 +32,14 @@ async function scrapCredly(browser: Browser) {
 
     await page.goto(FARAZ_AHMAD_CREDLY, { waitUntil: "load" });
 
-
+    // Waiting for some elements to be loaded
     await page.waitForSelector(CREDLY_CERTIFICATION_CONTAINER_SELECTOR);
     await page.waitForSelector(CREDLY_CERTIFICATION_IMAGE_SELECTOR);
     await page.waitForSelector(CREDLY_CERTIFICATION_NAME_SELECTOR);
 
     console.log("Credly website stuff loaded.");
 
-
+    // Obtaining every badge
     const badges = await page.$$eval(
         CREDLY_CERTIFICATION_CONTAINER_SELECTOR,
         (elements, { imageClass, nameClass }) => {
@@ -59,10 +63,12 @@ async function scrapCredly(browser: Browser) {
     console.log("Credly scrapping finished.");
     console.log(`${badges.length} badges were scrapped.`);
 
+    // Saving badges in a JSON file
     await fs.writeFile("webscrapping/credly_badges.json", JSON.stringify(badges), "utf8")
 }
 
 
+// Scrapping wallet badges
 async function scrapWallet(browser: Browser) {
 
     console.log("Starting wallet scrapping...");
@@ -75,13 +81,15 @@ async function scrapWallet(browser: Browser) {
     });
 
 
-    await page.waitForSelector(WALLET_CERTIFICATION_CONTAINER_SELECTOR);
-    await page.waitForSelector(WALLET_CERTIFICATION_IMAGE_SELECTOR);
-    await page.waitForSelector(WALLET_CERTIFICATION_NAME_SELECTOR);
-    await page.waitForSelector(WALLET_CERTIFICATION_LINK_SELECTOR);
+    // Waiting for some elements to be loaded
+    await page.waitForSelector(ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR);
+    await page.waitForSelector(ACCREDIBLE_CERTIFICATION_IMAGE_SELECTOR);
+    await page.waitForSelector(ACCREDIBLE_CERTIFICATION_NAME_SELECTOR);
+    await page.waitForSelector(ACCREDIBLE_CERTIFICATION_LINK_SELECTOR);
 
     const BADGES_COUNT_STRING = await page.$eval('span[_ngcontent-ng-c2957083266]', el => el.textContent);
 
+    // Looking the badges count upper label, if it's detected, then wait until the badges number is equivalent
     if (BADGES_COUNT_STRING) {
         const BADGES_COUNT = parseInt(BADGES_COUNT_STRING.split(" ")[0], 10);
 
@@ -91,10 +99,11 @@ async function scrapWallet(browser: Browser) {
             while (true) {
 
                 await page.evaluate(() => {
+                    // Scroll down to allow the badges to be loaded
                     const scroller = document.querySelector(".mat-drawer-content.mat-sidenav-content");
                     scroller?.scrollTo(0, scroller?.scrollHeight)
                 })
-                const LOADED_BADGES = await page.$$eval(WALLET_CERTIFICATION_CONTAINER_SELECTOR, els => els.length);
+                const LOADED_BADGES = await page.$$eval(ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR, els => els.length);
 
                 if (LOADED_BADGES >= BADGES_COUNT) {
                     break;
@@ -118,10 +127,11 @@ async function scrapWallet(browser: Browser) {
     type Certifications = (CertificationsData)[];
 
     let CERTIFICATIONS: Certifications = await page.$$eval(
-        WALLET_CERTIFICATION_CONTAINER_SELECTOR,
+        ACCREDIBLE_CERTIFICATION_CONTAINER_SELECTOR,
         (elements, { imageSelector, nameSelector, linkSelector, customCoverSelector, screenshotCoverSelector }) => {
 
             return elements.map((certificationContainer, index) => {
+                //Getting constant elements
                 const NAME_ELEMENT = certificationContainer.querySelector(nameSelector);
                 const LINK_ELEMENT = certificationContainer.querySelector(linkSelector) as HTMLAnchorElement;
 
@@ -130,12 +140,17 @@ async function scrapWallet(browser: Browser) {
 
                 try {
 
+                    // Getting image url
+                    // Images in Accredible are quirky to get
+
                     let imageUrl: string | null = "";
                     let imageElement = certificationContainer.querySelector(imageSelector);
 
                     if (imageElement != null) {
+                        // Simple image getting
                         imageUrl = imageElement.getAttribute('xlink:href') ?? imageElement.getAttribute("src");
                     } else {
+                        // If couldn't the simple way, then try a custom certification way
                         imageElement = certificationContainer.querySelector(customCoverSelector);
 
                         if (imageElement != null) {
@@ -144,6 +159,9 @@ async function scrapWallet(browser: Browser) {
                             imageUrl = match ? match[1] : null;
 
                         } else {
+
+                            // Even if it's not a custom certification, it will be taken a screenshot.
+                            // So here we mark the target element for the screenshot
 
                             imageElement = certificationContainer.querySelector(screenshotCoverSelector);
 
@@ -172,15 +190,16 @@ async function scrapWallet(browser: Browser) {
 
         },
         {
-            customCoverSelector: WALLET_CERTIFICATION_CUSTOM_CREDENTIAL_COVER_ELEMENT,
-            imageSelector: WALLET_CERTIFICATION_IMAGE_SELECTOR,
-            nameSelector: WALLET_CERTIFICATION_NAME_SELECTOR,
-            linkSelector: WALLET_CERTIFICATION_LINK_SELECTOR,
-            screenshotCoverSelector: WALLET_CERTIFICATION_SCREENSHOT_COVER_SELECTOR
+            customCoverSelector: ACCREDIBLE_CERTIFICATION_CUSTOM_CREDENTIAL_COVER_ELEMENT,
+            imageSelector: ACCREDIBLE_CERTIFICATION_IMAGE_SELECTOR,
+            nameSelector: ACCREDIBLE_CERTIFICATION_NAME_SELECTOR,
+            linkSelector: ACCREDIBLE_CERTIFICATION_LINK_SELECTOR,
+            screenshotCoverSelector: ACCREDIBLE_CERTIFICATION_SCREENSHOT_COVER_SELECTOR
         }
     ) as Certifications;
 
 
+    // Here it verifies if some of the gotten certifications requires a screenshot
     for (const index in CERTIFICATIONS) {
         const cert = CERTIFICATIONS[index];
 
@@ -203,6 +222,7 @@ async function scrapWallet(browser: Browser) {
         }
     }
 
+    // Filtering possible null values
     CERTIFICATIONS = CERTIFICATIONS.filter(c => c !== null);
 
     console.log("Wallet scrapping finished.");
@@ -211,6 +231,7 @@ async function scrapWallet(browser: Browser) {
     await fs.writeFile("webscrapping/wallet_badges.json", JSON.stringify(CERTIFICATIONS), 'utf8');
 }
 
+// Main process
 async function main() {
     await fs.mkdir("webscrapping/", { recursive: true });
 
@@ -225,6 +246,8 @@ async function main() {
         processes.push(process(browser));
     }
 
+
+    // Verifying the given arguments. If nothing was given, then exit the process.
 
     if (execArgv.includes("--credly")) {
         startProcess(scrapCredly)
